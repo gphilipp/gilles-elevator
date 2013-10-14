2be75067c0f4aba7c6413fa7bc33840178c1c50c
Gilles Philippart <gilles.philippart@gmail.com>
2013-10-14 21:56:09 +0200

    Clojure Omnibus is now working

--- src/gilles_elevator/core.clj
+++ src/gilles_elevator/core.clj
@@ -4,10 +4,14 @@
             [compojure.route :refer [not-found]]))
 
 (def commands (atom nil))
+(defn go [direction]
+  (take 5 (cycle [[direction "OPEN" "CLOSE"]]))  
+)
 
 (defroutes app
-  (GET "/reset" [] (reset! commands (cycle (concat (repeat 6 "UP") (repeat 6 "DOWN")))))
-  (GET "/call" [] "called!")
+  (GET "/reset" [] (do (reset! commands (flatten (cycle [(go "UP") (go "DOWN")])))
+                      "Reset eveything" ))
+  (GET "/call" [] "")
   (GET "/go" [] "")
   (GET "/userHasEntered" [] "")
   (GET "/userHasExited" [] "")
@@ -16,8 +20,7 @@
                             action))
   (not-found "No page here !"))
 
-
-(defonce server (jetty/run-jetty #'app {:port 8081 :join? false}))
+(Defonce server (jetty/run-jetty #'app {:port 8081 :join? false}))
 
 ;(.stop server)
 ;(.start server)
